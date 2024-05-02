baa0d74c547d526259ba751b1b0f9b7ce9b914ef
Rishthegod <rishit.on.mail@gmail.com>
2024-05-01 22:39:12 -0700

    Final HW4

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -1,60 +1,132 @@
-#include <gtest/gtest.h>
-#include <string.h>
-
-#include <algorithm>
-
 #include "string.hpp"
+#include "gtest/gtest.h"
+#include <algorithm>
+#include <string.h>
 #include "alloc.hpp"
 
+//Test for static helper strdup()
+TEST(StringFunction, strdup){
+    const char* Teststr = "Testing this string Hello! &@";
+    char* result = String::strdup(Teststr);
+    EXPECT_NE(result, nullptr);
+    EXPECT_STREQ(result, Teststr);
+    char* result2 = String::strdup("");
+    EXPECT_STREQ(result2, "");
+}
+
+// Test for static helper method strlen()
 TEST(StringFunction, strlen) {
-    EXPECT_EQ(String::strlen(""), 0);
-    EXPECT_EQ(String::strlen("foo"), 3);
+  const char *str = "Hello";
+  EXPECT_EQ(String::strlen(str), 5);
 }
 
+// Test for static helper method strcpy()
 TEST(StringFunction, strcpy) {
-    char result[10];
-    EXPECT_EQ(String::strcpy(result, "foo"), result);
-    EXPECT_STREQ(result, "foo");
-
-    EXPECT_EQ(String::strcpy(result, "a"), result);
-    EXPECT_STREQ(result, "a");
-
-    EXPECT_EQ(String::strcpy(result, ""), result);
-    EXPECT_STREQ(result, "");
+  const char *src = "Hello";
+  char dest[10];
+  String::strcpy(dest, src);
+  EXPECT_STREQ(dest, "Hello");
 }
 
-TEST(StringFunction, strdup) {
-    EXPECT_TRUE(false);
-}
-
+// Test for static helper method strncpy()
 TEST(StringFunction, strncpy) {
-    EXPECT_TRUE(false);
+  const char *src = "Hello";
+  char dest[10];
+  String::strncpy(dest, src, 3);
+  EXPECT_STREQ(dest, "Hel");
 }
 
+// Test for static helper method strcat()
 TEST(StringFunction, strcat) {
-    EXPECT_TRUE(false);
+  char dest[12] = "Hello";
+  const char *src = " World";
+  String::strcat(dest, src);
+  EXPECT_STREQ(dest, "Hello World");
 }
 
+// Test for static helper method strncat()
 TEST(StringFunction, strncat) {
-    EXPECT_TRUE(false);
+  char dest[10] = "Hello";
+  const char *src = " World";
+  String::strncat(dest, src, 3);
+  EXPECT_STREQ(dest, "Hello Wo");
 }
 
+// Test for static helper method strcmp()
 TEST(StringFunction, strcmp) {
-    EXPECT_TRUE(false);
+  const char *str1 = "Hello";
+  const char *str2 = "Hello";
+  EXPECT_EQ(String::strcmp(str1, str2), 0);
+  const char *str3 = "world";
+  EXPECT_LT(String::strcmp(str1, str3), 0);
+  const char *str4 = "Apple"; 
+  EXPECT_GT(String::strcmp(str1, str4), 0);
 }
 
+// Test for static helper method strncmp()
 TEST(StringFunction, strncmp) {
-    EXPECT_TRUE(false);
+  const char *str1 = "Hello";
+  const char *str2 = "Hel";
+  const char* empty = "";
+  EXPECT_EQ(String::strncmp(str1, str2, 3), 0);
+  EXPECT_LT(String::strncmp("hello", "world", 3), 0);
+  EXPECT_GT(String::strncmp("world", "hello", 3), 0);
+  EXPECT_LT(String::strncmp(empty, "abc", 2), 0);
+  EXPECT_EQ(String::strncmp(str1, str2, -1), 0);
+  EXPECT_GT(String::strncmp(str1, str2, 6), 0);
+
+
+
+
 }
 
+// Test for static helper method reverse_cpy()
 TEST(StringFunction, reverse_cpy) {
-    EXPECT_TRUE(false);
+  const char *src = "Hello";
+  char dest[10];
+  String::reverse_cpy(dest, src);
+  EXPECT_STREQ(dest, "olleH");
 }
 
+// Test for static helper method strchr()
 TEST(StringFunction, strchr) {
-    EXPECT_TRUE(false);
+  const char *str = "Hello";
+  char c = 'e';
+  const char *result = String::strchr(str, c);
+  EXPECT_EQ(result, str + 1);
 }
 
+// Test for static helper method strstr()
 TEST(StringFunction, strstr) {
-    EXPECT_TRUE(false);
+  const char *haystack = "Hello World";
+  const char *needle = "World";
+  const char *result = String::strstr(haystack, needle);
+  EXPECT_EQ(result, haystack + 6);
+  const char *needle2 = "apple"; 
+  const char *result2 = String::strstr(haystack, needle2);
+  EXPECT_EQ(result2, nullptr);
+  const char *haystack3 = "";
+  const char *needle3 = "hello";
+  const char *result3 = String::strstr(haystack3, needle3);
+  EXPECT_EQ(result3, nullptr);
+
+  const char *haystack4 = "hello";
+  const char *needle4 = "";
+  const char *result4 = String::strstr(haystack4, needle4);
+  EXPECT_EQ(result4, haystack4);
+
+  const char *haystack5 = "hello";
+  const char *needle5 = "hello world";
+  const char *result5 = String::strstr(haystack5, needle5);
+  EXPECT_EQ(result5, nullptr);
+
+  const char *haystack6 = "hello!@#$%^world";
+  const char *needle6 = "!@#$%^";
+  const char *result6 = String::strstr(haystack6, needle6);
+  EXPECT_EQ(result6, haystack6 + 5);
+
+  const char *haystack7 = "hello world";
+  const char *needle7 = " ";
+  const char *result7 = String::strstr(haystack7, needle7);
+  EXPECT_EQ(result7, haystack7 + 5);
 }
