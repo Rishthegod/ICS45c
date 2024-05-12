a92ec3ece36fff8ae3aae7967e461e7a06405488
Rishthegod <rishit.on.mail@gmail.com>
2024-05-11 21:20:34 -0700

    submission commit

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -5,7 +5,7 @@
 Triangle::Triangle(Point center, std::string name, int base, int height): Shape(center, name), base(base), height(height){}
 
 double Triangle::area() const{
-  return (base*height) / 2;
+  return double((base*height) / 2);
 }
 
 Triangle *Triangle::clone() const { 
@@ -16,7 +16,7 @@ void Triangle::draw(std::ostream& out) const{
   int offset = std::max(height,base);
 
   for(int i = 0; i < height; i +=2){
-    for(int  j=  0; j < (offset -1) / 2 ; ++j){
+    for(int  j =  0; j < (offset -i) / 2 ; ++j){
       out << ' ';
     }
     for (int k =0;  k <= i; ++k){
